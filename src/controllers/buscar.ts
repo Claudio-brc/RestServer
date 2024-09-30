f2a3da3933377a2dfe86f02d06cdcad05fff3bab
calvarezflx <claudio.alvarez@flexxus.com.ar>
2024-09-30 02:01:29 -0300

    implementación búsquedas

--- src/controllers/buscar.ts
+++ src/controllers/buscar.ts
@@ -2,13 +2,68 @@ import { response, Response }  from "express";
 import { MyRequest } from "../types";
 import { isValidObjectId }  from "mongoose"
 import Usuario from "../models/usuario"
+import Categoria from "../models/categoria"
+import Producto from "../models/producto";
 
 const coleccionesPermitidas = [
     'usuarios',
-    'categoria',
+    'categorias',
     'productos',
     'roles'
 ]
+
+const buscarProductos = async (termino: string = '', res: Response) => {
+  const esMongoId = isValidObjectId ( termino);
+
+  if ( esMongoId ) {
+    const producto = await Producto.findById(termino);  
+    return res.json(
+      {
+          results: ( producto ) ? [producto] : []
+
+      })
+  }
+  const regex = new RegExp(termino, 'i');
+
+  const productos = await Producto.find({
+  
+    $or:  [{nombre: regex}],
+    $and: [{ estado: true } ]
+  
+  });
+
+  res.json({
+    results: productos
+  })
+}
+
+
+const buscarCategorias = async (termino: string = '', res: Response) => {
+  const esMongoId = isValidObjectId ( termino);
+
+  if ( esMongoId ) {
+    const categoria = await Categoria.findById(termino);  
+    return res.json(
+      {
+          results: ( categoria ) ? [categoria] : []
+
+      })
+  }
+  const regex = new RegExp(termino, 'i');
+
+  const categorias = await Categoria.find({
+  
+    $or:  [{nombre: regex}],
+    $and: [{ estado: true } ]
+ 
+  
+  });
+
+  res.json({
+    results: categorias
+  })
+}
+
 
 const buscarUsuarios = async (termino: string = '', res: Response) => {
     const esMongoId = isValidObjectId ( termino);
@@ -50,7 +105,12 @@ const buscar = (  req: MyRequest, res: Response ) => {
     case 'usuarios':
         buscarUsuarios(termino,res)
     break;
-    
+    case 'categorias':
+        buscarCategorias(termino,res)
+    break;  
+    case 'productos':
+        buscarProductos(termino,res)
+    break;        
     default:
         res.status(500).json(
             { msg: 'se me olvido hacer esta búsqueda.'}
